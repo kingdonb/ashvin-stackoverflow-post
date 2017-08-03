83b0a5d385118b988be3bc3d65c428a14119fd59
Kingdon Barrett <kbarret8@nd.edu>
2017-08-03 13:21:56 -0400

    more time on rails associations
    and a scope for current_unpaid

--- app/models/award_test.rb
+++ app/models/award_test.rb
@@ -1,6 +1,9 @@
 class AwardTest < ApplicationRecord
   belongs_to :award
   has_many :instructor_student_awards
+  has_many :award_test_payment_notifications
+  has_many :instructor_students,
+    through: :instructor_student_awards
 
   scope :current_admin_award_test, ->(
     end_date, begin_date, instructor_student_list) {
@@ -11,5 +14,12 @@ class AwardTest < ApplicationRecord
       where(instructor_student_awards:
             { instructor_student: instructor_student_list }
            )
+    }
+
+  scope :current_unpaid, ->(
+    end_date, begin_date, instructor_student_list) {
+    current_admin_award_test(end_date, begin_date, instructor_student_list).
+      left_outer_joins(:award_test_payment_notifications).
+        where(award_test_payment_notifications: { id: nil } )
     }
 end
--- app/models/instructor_student_award.rb
+++ app/models/instructor_student_award.rb
@@ -2,6 +2,8 @@ class InstructorStudentAward < ApplicationRecord
   belongs_to :award
   belongs_to :instructor_student
   belongs_to :award_test
+  has_many :award_test_payment_notifications,
+    through: :instructor_student
 
   delegate :award, to: :award_test
 end
