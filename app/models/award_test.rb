543c04d2737e4b7c9bc48806c874a589313aa25c
Kingdon Barrett <kbarret8@nd.edu>
2017-08-03 12:52:48 -0400

    make it a scope!

--- app/models/award_test.rb
+++ app/models/award_test.rb
@@ -1,4 +1,15 @@
 class AwardTest < ApplicationRecord
   belongs_to :award
   has_many :instructor_student_awards
+
+  scope :current_admin_award_test, ->(
+    end_date, begin_date, instructor_student_list) {
+      joins(instructor_student_awards: :instructor_student).
+      where("test_date <= ? AND test_date >= ?",
+            end_date.end_of_month,
+            begin_date.beginning_of_month).
+      where(instructor_student_awards:
+            { instructor_student: instructor_student_list }
+           )
+    }
 end
